039ca552a6a0582d04d1c489e6de2faf04c5277d
RobinBigus <robin.bigus@wbscodingschool.com>
2025-05-28 14:01:52 +0200

    Added minor changes to reactRouter-lecture

--- module05-build-apps-with-react/reactRouter-lecture/src/App.jsx
+++ module05-build-apps-with-react/reactRouter-lecture/src/App.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route } from "react-router";
-import Home from "./pages/Home";
+import { Route, Routes } from "react-router";
+import Layout from "./layouts/Layout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Users from "./pages/Users";
+import Home from "./pages/Home";
 import UserProfile from "./pages/UserProfile";
-import Layout from "./layouts/Layout";
+import Users from "./pages/Users";
 
 const App = () => {
   return (
     <div className="routes">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:userId" element={<UserProfile />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="users" element={<Users />} />
+          <Route path="users/:userId" element={<UserProfile />} />
         </Route>
       </Routes>
     </div>
