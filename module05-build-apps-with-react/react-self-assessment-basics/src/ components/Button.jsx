2a8f97864b4281408a5e8c1b21c2d242c13c9bdb
RobinBigus <robin.bigus@wbscodingschool.com>
2025-06-06 12:01:22 +0200

    Added react-self-assessment-basics

---
+++ module05-build-apps-with-react/react-self-assessment-basics/src/ components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  // TODO: Render a <button> element
+  // TODO: Use the `label` prop as the button's text
+  // TODO: Call the `onClick` handler when clicked
+  // TODO: Apply the `disabled` prop if provided
+  return null;
+};
+
+export default Button;
